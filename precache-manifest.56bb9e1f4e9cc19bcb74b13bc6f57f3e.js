14dbf12460e2e04e38fe702ed439b7fa6f842aa5
Atul1023 <07atul1023@gmail.com>
2020-10-19 18:33:54 +0530

    Updates

---
+++ precache-manifest.56bb9e1f4e9cc19bcb74b13bc6f57f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48afb51a27cde400f78f485aaa27acbc",
+    "url": "/react-navbar/index.html"
+  },
+  {
+    "revision": "3a0512ab0eb7d3329886",
+    "url": "/react-navbar/static/css/main.b43b4117.chunk.css"
+  },
+  {
+    "revision": "85787e01380710def843",
+    "url": "/react-navbar/static/js/2.ef5f21b0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-navbar/static/js/2.ef5f21b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0512ab0eb7d3329886",
+    "url": "/react-navbar/static/js/main.c4958519.chunk.js"
+  },
+  {
+    "revision": "d068d03df11fcde21e97",
+    "url": "/react-navbar/static/js/runtime-main.315b024a.js"
+  }
+]);
