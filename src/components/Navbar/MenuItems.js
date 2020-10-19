8bce4f2e521ab8944743aac077e8609704b9bf60
Atul1023 <07atul1023@gmail.com>
2020-10-19 18:27:14 +0530

    Pushing code to repo

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#home',
+        cName: 'nav-links', 
+    },
+    {
+        title: 'About',
+        url: '#about',
+        cName: 'nav-links', 
+    },
+    {
+        title: 'Products',
+        url: '#products',
+        cName: 'nav-links', 
+    },
+    {
+        title: 'Testimonials',
+        url: '#testimonials',
+        cName: 'nav-links', 
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links', 
+    },
+]
